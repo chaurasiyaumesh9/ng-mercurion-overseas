1c4fcc550505a8dc2d89109b71aba6aa431a79b4
Umesh C <uchaurasiya@girsoftwareservices.com>
2026-02-26 23:04:35 +0530

    routing restructuring

--- src/app/shopping/shopping.routes.ts
+++ src/app/shopping/shopping.routes.ts
@@ -1,39 +1,51 @@
 import { Routes } from '@angular/router';
 
 export const shoppingRoutes: Routes = [
+  // HOME
   {
     path: '',
-    children: [
-      {
-        path: 'cart',
-        loadComponent: () => import('./components/cart/cart').then((m) => m.Cart),
-      },
-      {
-        path: '',
-        loadComponent: () => import('./components/home/home').then((m) => m.Home),
-      },
-      {
-        path: 'product/:sku',
-        loadComponent: () => import('./components/product-details/product-details').then((m) => m.ProductDetails),
-      },
-      {
-        path: '',
-        children: [
-          {
-            path: 'search',
-            loadComponent: () => import('./components/product-listing/product-listing').then((m) => m.ProductListing),
-          },
-          {
-            path: ':categorySlug',
-            loadComponent: () => import('./components/product-listing/product-listing').then((m) => m.ProductListing),
-          },
-          {
-            path: ':categorySlug/:subCategorySlug',
-            loadComponent: () => import('./components/product-listing/product-listing').then((m) => m.ProductListing),
-          },
-        ],
-      },
-      
-    ],
+    loadComponent: () => import('./components/home/home').then((m) => m.Home),
+  },
+
+  // PRODUCT
+  {
+    path: 'product/:sku',
+    loadComponent: () =>
+      import('./components/product-details/product-details').then((m) => m.ProductDetails),
+  },
+
+  // CART
+  {
+    path: 'cart',
+    loadComponent: () => import('./components/cart/cart').then((m) => m.Cart),
+  },
+
+  // SEARCH + FACETS
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./components/product-listing/product-listing').then((m) => m.ProductListing),
+  },
+  {
+    path: 'search/**',
+    loadComponent: () =>
+      import('./components/product-listing/product-listing').then((m) => m.ProductListing),
+  },
+
+  // CATEGORY + FACETS
+  {
+    path: ':categorySlug',
+    loadComponent: () =>
+      import('./components/product-listing/product-listing').then((m) => m.ProductListing),
+  },
+  {
+    path: ':categorySlug/:subCategorySlug',
+    loadComponent: () =>
+      import('./components/product-listing/product-listing').then((m) => m.ProductListing),
+  },
+  {
+    path: ':categorySlug/:subCategorySlug/**',
+    loadComponent: () =>
+      import('./components/product-listing/product-listing').then((m) => m.ProductListing),
   },
 ];
